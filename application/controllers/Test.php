697eb3067d82b3f809d7026b82a2c9e618fb2e6e
projects.mindiii@gmail.com <projects.mindiii@gmail.com>
2019-07-11 16:14:41 +0530

    9th commit

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,38 +1,26 @@
-
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Test extends CI_Controller {
- 
-    public function index()
-    {
-       error_reporting(E_ALL);
-ini_set("display_errors", 1);
-        $phoneNumber = "+919009585194";
-       // $phoneNumber = "+918319990292";
-        $text = "http://reservision.org check number";
-         
-			
-            $this->load->library('text_sms');
-            $response=$this->text_sms->sendSMS($phoneNumber,$text);
-            
-            echo json_encode($response); 
-//~ $this->load->library('background');
-//~ $url = base_url()."manage/textSms";
-//~ $param = array('phoneNumber' => "9009585194",'countryCode' => "+91",'message'=>"test background" );
-              
- 
-               //~ $this->background->do_in_background($url, $param);
-               //~ echo "hiii";
-    }
-    function checkNumber(){
-		  $this->load->library('text_sms');
-		  $phoneNumber='+919009585194';
-            $response=$this->text_sms->isValidNumber($phoneNumber);
-            pr($response);
-		
-	}
- 
+<?php 
+ /**
+ * 
+ */
+ class Test extends CI_Controller
+ {
   
-}
-//Qs+1OdHa!tiB
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+  function test(){
+    $this->load->library('background');
+        $subject = 'New Service';
+        $email   =  "php.vaibhav@gmail.com";
+        $urlMail            = base_url()."manage/mailSent";
+        $maildata['title'] ="test demo";
+        $maildata['message'] ="mail send successfully";
+        $paramMail = array('email' =>$email,'subject' =>$subject,'path'=>'email','msg'=>$maildata);
+
+        $this->background->do_in_background($urlMail, $paramMail);
+        echo "done";
+  }
+ }
+ ?>
