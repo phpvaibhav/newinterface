49274046b79f4fd72fb077e6e38060d628616bac
P@55word <php.vaibhav@gmail.com>
2019-07-25 17:30:13 +0530

    26th commit

--- application/modules/api/controllers/Service.php
+++ application/modules/api/controllers/Service.php
@@ -146,7 +146,9 @@ class Service extends Common_Service_Controller{
             }
 
             $linkDtail = base_url('service/serviceDetail/'.encoding($serData->serviceId));
-            $action .= '<a href="'.$link.'" onclick="statusChange(this);" data-message="You want to change status!" data-serid="'.encoding($serData->serviceId).'" data-sid="'.encoding($applyStatus).'"  class="on-default edit-row table_action" title="View user">'.$applyMsg.'</a>';  
+            if($userType==1):
+            $action .= '<a href="'.$link.'" onclick="statusChange(this);" data-message="You want to change status!" data-serid="'.encoding($serData->serviceId).'" data-sid="'.encoding($applyStatus).'"  class="on-default edit-row table_action" title="View user">'.$applyMsg.'</a>';
+            endif;
             $action .= '&nbsp;<a href="'.$linkDtail.'" class="on-default edit-row table_action" title="Detail"><i class="fa fa-eye"></i></a>';
                
             // $clk_edit =  "editFn('admin/categoryCtrl','editGenres','$usersData->id');" ;
